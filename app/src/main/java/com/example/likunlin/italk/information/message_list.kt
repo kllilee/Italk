08b9a6dfbfe3cafd82172fcc1172b007e7ab7c40
Kllilee <kllilee21@gmail.com>
2018-05-02 12:50:16 +0800

    Italk version test

--- app/src/main/java/com/example/likunlin/italk/information/message_list.kt
+++ app/src/main/java/com/example/likunlin/italk/information/message_list.kt
@@ -30,7 +30,7 @@ class message_list: ArrayList<message_info>() {
         message_info_1.add(message_info(name = "Hury",time = "2018/03/16",message = "test2",img_name = "img_path",chanel = "admin01"))
         message_info_1.add(message_info(name = "Ted",time = "2018/03/16",message = "test3",img_name = "img_path",chanel = "admin01"))
         message_info_1.add(message_info(name = "York",time = "2018/03/16",message = "test4",img_name = "img_path",chanel = "admin01"))
-        message_info_1.add(message_info(name = "Ted",time = "2018/03/16",message = "test5",img_name = "img_path",chanel = "admin01"))
+
 
 
         return message_info_1
